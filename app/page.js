35cea260943aeaa53524a09f20ad80f5fba535f6
Sahil 2006 <sahilshabbirharyani@gmail.com>
2025-08-31 15:27:42 +0300

    Test changes

--- app/page.js
+++ app/page.js
@@ -5,7 +5,8 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="text-center space-y-4">
         <h1 className="text-4xl font-bold">Ossome Hacks 3.0</h1>
-        <p className="text-lg">We are glad to have you here. Explore our content and enjoy your stay!</p>
+        <p className="text-lg">We are glad to have you here.Hey Just </p>
+         <p>explore our content and enjoy your stay!</p>
       </div>
     </div>
   );
